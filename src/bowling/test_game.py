15b69632f2bb320416359f257861c073406d16ba
PedroGuerraMP <pedrowar.pw@gmail.com>
2023-03-29 23:12:10 -0300

    [+] add test_get_finished_game_score

--- src/bowling/test_game.py
+++ src/bowling/test_game.py
@@ -109,5 +109,32 @@ class TestGames(unittest.TestCase):
             
         self.assertTrue('Game already finished', context.exception)
         
+    def test_get_finished_game_score(self):
+        game = BowlingGame() 
+        frame1 = Frame(1, 2)
+        frame2 = Frame(1, 2)
+        frame3 = Frame(1, 2)
+        frame4 = Frame(1, 2)
+        frame5 = Frame(1, 2)
+        frame6 = Frame(1, 2)
+        frame7 = Frame(1, 2)
+        frame8 = Frame(1, 2)
+        frame9 = Frame(1, 2)
+        frame10 = Frame(1, 2)
+                
+        game.add_frame(frame1)
+        game.add_frame(frame2)
+        game.add_frame(frame3)
+        game.add_frame(frame4)
+        game.add_frame(frame5)
+        game.add_frame(frame6)
+        game.add_frame(frame7)
+        game.add_frame(frame8)
+        game.add_frame(frame9)
+        game.add_frame(frame10)
+        
+        self.assertEqual(game.score(),30) 
+        
+        
 if __name__ == '__main__':
     unittest.main()
