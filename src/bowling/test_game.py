745751d45160e58c67489606c723f7d9ea9d2594
PedroGuerraMP <pedrowar.pw@gmail.com>
2023-03-29 22:02:43 -0300

    [+] test_add_frame

--- src/bowling/test_game.py
+++ src/bowling/test_game.py
@@ -1,8 +1,18 @@
 import unittest
+from bowling.frame import Frame
+from bowling.game import BowlingGame
 
 
 class TestGames(unittest.TestCase):
-    pass
+    
+    def test_add_frame(self):
+        game = BowlingGame()
+        frame = Frame(1, 2)
+        
+        game.add_frame(frame)
+        
+        self.assertTrue( game.frames.index(frame) >= 0 )
+
 
 if __name__ == '__main__':
     unittest.main()
