9eaa7d9055298639efeac46e0c5520355401be1a
PedroGuerraMP <pedrowar.pw@gmail.com>
2023-03-29 22:36:26 -0300

    [+] add test_get_game

--- src/bowling/test_game.py
+++ src/bowling/test_game.py
@@ -24,5 +24,34 @@ class TestGames(unittest.TestCase):
         self.assertTrue( game.frames.index(frame1) >= 0 )
         self.assertTrue( game.frames.index(frame2) >= 0 )
 
+    def test_get_game(self):
+        game = BowlingGame()        
+        frame1 = Frame(1, 2)
+        frame2 = Frame(1, 2)
+        frame3 = Frame(1, 2)
+        frame4 = Frame(1, 2)
+        frame5 = Frame(1, 2)
+        frame6 = Frame(1, 2)
+        frame7 = Frame(1, 2)
+        frame8 = Frame(1, 2)
+        frame9 = Frame(1, 2)
+        frame10 = Frame(1, 2)
+        
+        frameList = [frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10]
+        
+        game.add_frame(frame1)
+        game.add_frame(frame2)
+        game.add_frame(frame3)
+        game.add_frame(frame4)
+        game.add_frame(frame5)
+        game.add_frame(frame6)
+        game.add_frame(frame7)
+        game.add_frame(frame8)
+        game.add_frame(frame9)
+        game.add_frame(frame10)
+        
+        self.assertEqual(game.get_game().equals, frameList)        
+
+
 if __name__ == '__main__':
     unittest.main()
